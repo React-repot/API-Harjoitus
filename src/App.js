4cdc246b227b9dee0e6f810d9c4bd33afb6ade9b
Aku Nikula <aku.nikula@gmail.com>
2023-02-27 17:57:01 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-const URL = ""
-const API_KEY = "ÄLÄ LAITA SITÄ GITHUBIIN"
+const URL = "https://randomfox.ca/floof/"
+
 
 function App() {
+  const [image, setImage] = useState("")
+  const [imageLink, setImageLink] = useState("")
+  const [isLoading, setIsLoading] = useState(true)
 
-  
-  return (
-    <>
-    
-    </>
-  );
+  useEffect(() => {
+
+    axios.get(URL)
+      .then((response) => {
+        setImage(response.data.image)
+        setImageLink(response.data.link)
+        setIsLoading(false)
+      }).catch(error => {
+        alert(error)
+      })
+  }, [])
+
+  if (isLoading) {
+    return <h3>Loading your fox...</h3>
+  }
+  else {
+    return (
+      <div>
+        <h1>Welcome to the foxhole</h1>
+        <img src={image} />
+        <p>Do you enjoy your fox? Here are more options:</p>
+        <a href={imageLink}>Full size</a>
+        <a href="">New fox</a>
+      </div>
+    );
+  }
 }
 
 export default App;
